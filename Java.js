7f2105ecb156f3a0d1a59fe3f170ef8000eb5903
Jovanna S <147216237+Jojosleeps@users.noreply.github.com>
2025-05-09 10:20:46 -0400

    Add files via upload

---
+++ Java.js
@@ -0,0 +1,28 @@
+window.addEventListener("load",InitControls);
+window.addEventListener("load",addListener);
+
+function InitControls()
+{
+	document.getElementById("txtonesele").textContent = "";
+	document.getElementById("txttwosele").textContent = "";
+	document.getElementById("txtthirdsele").textContent = "";
+	document.getElementById("txtfoursele").focus();
+}
+
+function addListener()
+{
+	document.getElementById("btnsubmit").addEventListener("click",Check);
+}
+
+function Check()
+{
+	var 
+}
+
+
+
+
+
+
+
+
