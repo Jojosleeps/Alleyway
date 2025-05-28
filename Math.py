541d9f9389a4d7b1a8f2a4ae67a62f30d1a1d190
Jovanna S <147216237+Jojosleeps@users.noreply.github.com>
2025-05-28 10:16:04 -0400

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os.path
+from os import path
+
+def main():
+    AskInfo();
+
+def AskInfo():
+    
+
+if __name__ == "__main__":
+    main();
